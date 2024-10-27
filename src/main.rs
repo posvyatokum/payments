2b49a3b4a6f3d8bc50295293cb9ca79be41900ea
posvyatokum <posvyatokum@edu.hse.ru>
2024-10-27 15:32:05 +0000

    enable logging

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,18 @@ mod types;
 use log::info;
 
 fn main() {
+    let _ = env_logger::try_init();
     let args: Vec<String> = std::env::args().collect();
     match args.get(1) {
         None => {
-            info!("Reading data from stdin.");
+            info!(target: "main", "Reading data from stdin.");
             flow::output_csv_clients(
                 flow::process_csv_transactions(std::io::stdin()),
                 std::io::stdout(),
             );
         }
         Some(filename) => {
-            info!("Reading data from {filename}");
+            info!(target: "main", "Reading data from {filename}");
             let reader = std::io::BufReader::new(std::fs::File::open(filename).unwrap());
             flow::output_csv_clients(flow::process_csv_transactions(reader), std::io::stdout());
         }
